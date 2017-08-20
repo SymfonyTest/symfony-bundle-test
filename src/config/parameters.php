4eeded2e216469e4b49f59922301624f382eb4cb
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-20 10:03:24 +0200

    Applied changes from StyleCI

--- src/config/parameters.php
+++ src/config/parameters.php
@@ -1,7 +1,7 @@
 <?php
 
 if (\Symfony\Component\HttpKernel\Kernel::VERSION_ID >= 30300) {
-    $container->loadFromExtension('framework', array(
+    $container->loadFromExtension('framework', [
         'annotations' => ['enabled' => false],
-    ));
+    ]);
 }
