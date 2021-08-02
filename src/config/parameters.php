408e6cff759099665efb1e667df28d731c38dcfe
chapterjason <jason@sourecode.dev>
2021-08-02 08:05:41 +0200

    chore: Several version related changes

--- src/config/parameters.php
+++ src/config/parameters.php
@@ -1,6 +1,6 @@
 <?php
 
-// If sf 3.3 and no annotations is installed, then disable it.
+// If symfony is greater than 3.3 and no annotations are installed, then disable it.
 if (\Symfony\Component\HttpKernel\Kernel::VERSION_ID >= 30300 && !class_exists('Doctrine\Common\Annotations\Annotation')) {
     $container->loadFromExtension('framework', [
         'annotations' => ['enabled' => false],
